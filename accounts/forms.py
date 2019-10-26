d8769d08a5b9f5b5bd4a05aa4712d5e9999aa2f0
kynefuk <atsuminamihira@gmail.com>
2019-10-26 15:46:55 +0900

    [mod] ユーザ登録フォームのフィールドにヘルプテキストを追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,14 +10,18 @@ class CustomSignupForm(SignupForm):
     """
     allauthの非ソーシャルログイン用のフォームクラス
     """
-    profile_pic = forms.FileField(label=('プロフィール画像'), required=False)
+
+    username = forms.CharField(label='ユーザ名', required=True, help_text='※必須')
+    email = forms.EmailField(label='Eメール', required=True, help_text='※必須')
+    profile_pic = forms.FileField(label='プロフィール画像', required=False, help_text='※任意')
 
     def save(self, request):
         """
         「user」を最後にreturnする必要がある。
         内部的にはadapterクラスを呼び出し、adapterクラスのsave_userを呼び出している。
         """
-        user = super(CustomSignupForm, self).save(request)
+
+        user = super().save(request)
         return user
 
 
@@ -26,14 +30,17 @@ class CustomSocialSignupForm(SocialSignupForm):
     ソーシャルログイン用のフォームクラス
     """
 
-    profile_pic = forms.FileField(label=('プロフィール画像'), required=False)
+    username = forms.CharField(label='ユーザ名', required=True, help_text='※必須')
+    email = forms.EmailField(label='Eメール', required=True, help_text='※必須')
+    profile_pic = forms.FileField(label='プロフィール画像', required=False, help_text='※未選択の場合は、ソーシャルアカウントのプロフィール画像を設定します')
 
     def save(self, request):
         """
         「user」を最後にreturnする必要がある。
         内部的にはadapterクラスを呼び出し、adapterクラスのsave_userを呼び出している。
         """
-        user = super(CustomSocialSignupForm, self).save(request)
+
+        user = super().save(request)
         return user
 
 
@@ -51,7 +58,7 @@ class CustomUserUpdateForm(forms.ModelForm):
         表示させないよう、widgetをFileInputに変更
         """
 
-        super(CustomUserUpdateForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.fields['profile_pic'].widget = FileInput()
 
 
